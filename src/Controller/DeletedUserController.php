26011364d775cbfb1bc4d93501c000b8617b9f1f
Padariukas <zvirblisdarius@yahoo.com>
2019-04-30 10:12:19 +0300

    Master upgrade

--- src/Controller/DeletedUserController.php
+++ src/Controller/DeletedUserController.php
@@ -39,7 +39,7 @@ class DeletedUserController extends Controller
         $pagination = $paginator->paginate(
             $query, /* query NOT result */
             $request->query->getInt('page', 1), /*page number*/
-            5 /*limit per page*/
+            20 /*limit per page*/
         );
 
         // parameters to template
